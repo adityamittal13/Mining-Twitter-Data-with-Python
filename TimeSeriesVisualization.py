d5f7df5c6ba3d19ed778c1caab381915df3b619b
Aditya Mittal <39227064+adityamittal13@users.noreply.github.com>
2020-12-07 07:08:03 -0800

    Add files via upload

---
+++ TimeSeriesVisualization.py
@@ -0,0 +1,65 @@
+# OLD PREPROCESS CODE
+# Splits up the tweet into hashtags, emoticons, the @ tags, words, punctuation, website URLs
+import re
+import json
+
+# Regex expressions for emoticons 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+ # Regex expressions for the anatomy of a tweet
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+
+# Compilation of tokens and emoticons (making it so spaces are ignroed with re.VERBOSE and catch upper and lowercase with re.IGNORECASE)
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+# Finds all of the parts of s that correspond with regex expressions
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+# First, finds a list of tokens using tokenize() - if it's not an emoticon, it makes it lowercase
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+import pandas
+import json
+ 
+dates_ITAvWAL = []
+# f is the file pointer to the JSON data set
+with open('#pythonStreamingData.json', 'r') as f:
+    for line in f:
+        tweet = json.loads(line)
+        # let's focus on hashtags only at the moment
+        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#')]
+        # track when the hashtag is mentioned
+        if '#itavwal' in terms_hash:
+            dates_ITAvWAL.append(tweet['created_at'])
+     
+# a list of "1" to count the hashtags
+ones = [1]*len(dates_ITAvWAL)
+# the index of the series
+idx = pandas.DatetimeIndex(dates_ITAvWAL)
+# the actual series (at series of 1s for the moment)
+ITAvWAL = pandas.Series(ones, index=idx, dtype = 'float64')
+ 
+# Resampling / bucketing
+per_minute = ITAvWAL.resample('1Min').sum().fillna(0)
